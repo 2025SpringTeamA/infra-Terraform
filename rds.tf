459ab965f97d50d9213cd7ab7b2e087b51f28c3e
ARISA1115 <a.engine0808@gmail.com>
2025-05-14 01:16:33 +0900

    [add]rds.tf

--- rds.tf
+++ rds.tf
@@ -3,41 +3,26 @@
 
 # RDS 用のサブネットグループの定義
 resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = "terraform-rds-subnet-group"
+  name       = "${var.project_prefix}-rds-subnet-group"
   subnet_ids = [
     aws_subnet.private3.id,
     aws_subnet.private4.id
   ]
 
   tags = {
-    Name = "Terraform-rds-subnet-group"
+    Name = "${var.project_prefix}-rds-subnet-group"
   }
-}
-
-# Secrets Manager に保存する RDS パスワード用のシークレットを作成
-resource "aws_secretsmanager_secret" "rds_password_v2" {
-  name = "rds_password_v2"
-}
-
-# シークレットの初期バージョン（パスワードを定義）
-resource "aws_secretsmanager_secret_version" "rds_password_version_v2" {
-  secret_id     = aws_secretsmanager_secret.rds_password_v2.id
-  secret_string = jsonencode({ password = "mypassword123" })
-}
-
-# Secrets Manager からパスワードを取得
-data "aws_secretsmanager_secret_version" "rds_password_v2" {
-  secret_id  = aws_secretsmanager_secret.rds_password_v2.arn
-  depends_on = [aws_secretsmanager_secret_version.rds_password_version_v2]
 }
 
 # RDS インスタンスの定義（MySQL 8.0）
 resource "aws_db_instance" "rds_instance" {
+  identifier           = "${var.project_prefix}-rds"
+  engine               = "mysql"
+  engine_version       = "8.0.41"
+  instance_class       = "db.t4g.micro"
   allocated_storage    = 20
   storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "8.0.39"
-  instance_class       = "db.t4g.micro"
+  db_name              = var.rds_db_name
 
   # 認証情報
   username = "admin"
@@ -51,7 +36,7 @@ resource "aws_db_instance" "rds_instance" {
   multi_az            = false
 
   tags = {
-    Name = "Terraform-rds-instance"
+    Name = "${var.project_prefix}-rds-instance"
   }
 
   # 削除時にスナップショットを作成しない
