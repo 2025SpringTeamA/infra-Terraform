4c44b8b070edb56bfb383c7d161039a27ac33a97
ARISA1115 <a.engine0808@gmail.com>
2025-05-14 01:14:31 +0900

    [add]alb.tf

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@
 
 # アプリケーションロードバランサー（ALB）の定義
 resource "aws_lb" "main_alb" {
-  name               = "terraform-main-alb"
+  name               = "${var.project_prefix}-main-alb"
   internal           = false                               # パブリック ALB として構成
   load_balancer_type = "application"
   security_groups    = [aws_security_group.public_sg.id]   # ALB に適用するセキュリティグループ
@@ -15,14 +15,14 @@ resource "aws_lb" "main_alb" {
   enable_deletion_protection = false                       # 削除保護を無効化
 
   tags = {
-    Name = "terraform-main-alb"
+    Name = "${var.project_prefix}-main-alb"
   }
 }
 
 # ターゲットグループの定義（ALB がリクエストを転送する先）
 resource "aws_lb_target_group" "main_target_group" {
-  name        = "terraform-main-target-group"
-  port        = 80
+  name        = "${var.project_prefix}-main-target-group"
+  port        = 8000
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"                                       # Fargate の場合は 'ip' を指定
@@ -45,7 +45,7 @@ resource "aws_lb_target_group" "main_target_group" {
   }
 
   tags = {
-    Name = "terraform-main-target-group"
+    Name = "${var.project_prefix}-main-target-group"
   }
 }
 
@@ -55,7 +55,7 @@ resource "aws_lb_listener" "http_listener" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.acm_certificate_arn              # ACM 証明書の ARN を指定
+  certificate_arn   = var.acm_certificate_arn_ap_northeast_1              # ACM 証明書の ARN を指定
 
   default_action {
     type             = "forward"
