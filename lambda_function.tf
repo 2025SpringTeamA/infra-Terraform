f91db7932e15acd3902a3520514c622313c4ec44
ARISA1115 <a.engine0808@gmail.com>
2025-05-31 12:19:06 +0900

    [fix]Lambda用の実行時変数を locals で定義

--- lambda_function.tf
+++ lambda_function.tf
@@ -1,4 +1,4 @@
-# 必要なリソースから値を取得して変数に代入（変数を使わず、localsで管理）
+# Lambda用の実行時変数を locals で定義
 locals {
   ecs_cluster_name         = aws_ecs_cluster.main.name
   ecs_task_definition_name = aws_ecs_task_definition.main.family
@@ -26,9 +26,9 @@ resource "aws_lambda_function" "ecs_runtask_lambda" {
 
   environment {
     variables = {
-      ECS_CLUSTER_NAME     = var.ecs_cluster_name
-      ECS_TASK_DEFINITION  = var.ecs_task_definition_name
-      SUBNETS              = join(",", var.private_subnet_ids)
+      ECS_CLUSTER_NAME     = local.ecs_cluster_name
+      ECS_TASK_DEFINITION  = local.ecs_task_definition_name
+      SUBNETS              = join(",", local.private_subnet_ids)
       SECURITY_GROUPS      = join(",", [aws_security_group.ecs_sg.id])
       ASSIGN_PUBLIC_IP     = "false"
     }
