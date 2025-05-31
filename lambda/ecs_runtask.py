2a4a06105acaf1b18788ff173d0997a0138a3f34
ARISA1115 <a.engine0808@gmail.com>
2025-05-31 12:01:44 +0900

    [add]lambda/ecs_runtask.py

---
+++ lambda/ecs_runtask.py
@@ -0,0 +1,21 @@
+import os
+import boto3
+
+def lambda_handler(event, context):
+    ecs = boto3.client('ecs')
+
+    response = ecs.run_task(
+        cluster=os.environ['ECS_CLUSTER_NAME'],
+        taskDefinition=os.environ['ECS_TASK_DEFINITION'],
+        launchType='FARGATE',
+        networkConfiguration={
+            'awsvpcConfiguration': {
+                'subnets': os.environ['SUBNETS'].split(','),
+                'securityGroups': os.environ['SECURITY_GROUPS'].split(','),
+                'assignPublicIp': 'ENABLED' if os.environ.get('ASSIGN_PUBLIC_IP', 'false').lower() == 'true' else 'DISABLED'
+            }
+        }
+    )
+
+    print("Task started successfully:", response)
+    return { "status": "invoked", "detail": response}
